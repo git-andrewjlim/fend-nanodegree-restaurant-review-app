aacc2ec77fd78e9e9c30ac1ffbab32e1c10a9fed
Andrew <andrewjlim@icloud.com>
2019-03-05 18:03:41 +1100

    added relative links to cached files

--- sw.js
+++ sw.js
@@ -5,13 +5,12 @@ self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open(cacheName).then(function(cache){
 			return cache.addAll([
-				'/',
-                'js/main.js',
-                'js/dbhelper.js',
-                'js/restaurant_info.js',
-                'css/styles.css',
-                'data/restaurants.json',
-				'img/simple-gray-texture.jpg'
+                './js/main.js',
+                './js/dbhelper.js',
+                './js/restaurant_info.js',
+                './css/styles.css',
+                './data/restaurants.json',
+				'./img/simple-gray-texture.jpg'
 			]);
 		}).catch(function(err){
             console.log('Service Worker install failure', err);
