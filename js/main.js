f781e518994ef0f1d364ae147d2878dd9bb5ace4
Andrew <andrewjlim@icloud.com>
2019-03-05 19:49:06 +1100

    updated the restaurant header to a h2

--- js/main.js
+++ js/main.js
@@ -153,7 +153,7 @@ createRestaurantHTML = (restaurant) => {
   image.src = `./img/${restaurant.photograph}-small.jpg`;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
